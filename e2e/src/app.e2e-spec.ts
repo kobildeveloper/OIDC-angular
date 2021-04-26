edd273b2cbb20923d21901851bbd01a0eb27e983
KOBIL <62881519+kobildeveloper@users.noreply.github.com>
2021-04-26 10:53:20 +0200

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('midentityone-implicitflow app is running!');
+    expect(page.getTitleText()).toEqual('midentitybox-implicitflow app is running!');
   });
 
   afterEach(async () => {
